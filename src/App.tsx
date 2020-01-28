871c459cc788447f081e6fcee031a164bac74d40
Tomer <tomerst3@gmail.com>
2020-01-28 18:39:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
     <div className="App">
       <div className="row">
         <div className="col-12">Hello World!</div>
-      </div>
+      </div> 
     </div>
   );
 }
