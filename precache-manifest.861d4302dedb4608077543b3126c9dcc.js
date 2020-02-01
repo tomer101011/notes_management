e18ee198a27a5ec814509ca08d34580c45f3b762
Tomer <tomerst3@gmail.com>
2020-02-01 23:43:12 +0200

    Updates

---
+++ precache-manifest.861d4302dedb4608077543b3126c9dcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f045913402e5689665e087b45385cd2e",
+    "url": "/notes_management/index.html"
+  },
+  {
+    "revision": "83a8a699f8bd7f39f79e",
+    "url": "/notes_management/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "010aefa60603c2777607",
+    "url": "/notes_management/static/css/main.9954076f.chunk.css"
+  },
+  {
+    "revision": "83a8a699f8bd7f39f79e",
+    "url": "/notes_management/static/js/2.2225c3ca.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/notes_management/static/js/2.2225c3ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "010aefa60603c2777607",
+    "url": "/notes_management/static/js/main.0937d84a.chunk.js"
+  },
+  {
+    "revision": "e5bf6efab7eda569badc",
+    "url": "/notes_management/static/js/runtime-main.d274b5aa.js"
+  },
+  {
+    "revision": "79c96a45cf274d54cfa372d072859584",
+    "url": "/notes_management/static/media/closeIcon.79c96a45.png"
+  }
+]);
